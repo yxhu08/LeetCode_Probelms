52b109522430b7e595dab4d3807b74f4c58611d0
Yashvi Bansal <yashvibansal2308@gmail.com>
2026-04-03 17:09:31 +0530

    Time: 0 ms (100%), Space: 17.9 MB (20.89%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.c
@@ -0,0 +1,13 @@
+void reverseString(char* s, int sSize) {
+    int left=0;
+    int right=sSize-1;
+    while(left<right)
+    {
+        char temp=s[left];
+        s[left]=s[right];
+        s[right]=temp;
+        left++;
+        right--;
+    }
+    
+}
